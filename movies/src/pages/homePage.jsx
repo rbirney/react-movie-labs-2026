b212cf88be944510f335a798916112272c2ac352
rbirney <rbirney@wit.ie>
2026-02-04 12:23:35 +0000

    test change

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -7,7 +7,7 @@ const HomePage = (props) => {
   return (
   <Grid container>
       <Grid size={12}>
-          <h1> HomePage </h1>
+          <h1> HomePage test </h1>
       </Grid>
     </Grid>
   );
